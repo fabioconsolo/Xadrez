aba91c2d0a81867487ccdc2ee57d505e3f8b5af8
Fabio Matarazzo <fabio.consolo@yahoo.com.br>
2022-12-06 12:05:17 -0300

    Criação ChessExecpiton e ChessPosition

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,36 +1,69 @@
 package boardgame;
 
 public class Board {
-	private int rows;
-	private int columns;
+	private int rows =8  ;
+	private int columns=8 ;
 	private Piece[][] pieces;
+
 	public Board(int row, int colums) {
-		
+
+		if (rows < 1 || colums < 1) {
+			throw new BoardExeption(
+		"Erro criando o tabuleiro , é necessario que tenha pelo menos 1 linha e 1 coluna");
+}
 		this.rows = row;
 		this.columns = colums;
 		pieces = new Piece[rows][columns];
 	}
+
 	public int getRows() {
 		return rows;
 	}
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
+
 	public int getColumns() {
 		return columns;
 	}
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
-	
-	
-	public Piece piece (int row , int column) {
-		  return pieces[row][column];
-		
-	}
-	public Piece piece(Position position) {
-		return pieces [position.getRow()][position.getColumn()];
-		
+
+	public Piece piece(int row, int column) {
+		if (!positionExists(row, column)) { 
+		throw new BoardExeption("Posição não existente");
+		}
+		return pieces[row][column];
+
 	}
 
+	public Piece piece(Position position) {
+		if (!PositionExists(position)) 
+			throw new BoardExeption("Posição não existente");
+		
+		return pieces[position.getRow()][position.getColumn()];
+
+	}
+
+	public void placePiece(Piece piece, Position position) {
+		if (ThereisAPiece(position)) {
+			throw new BoardExeption("Já tem uma peça na posição " + position);
+		}
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
+
+	}
+
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+
+	}
+
+	public boolean PositionExists(Position position) {
+
+		return positionExists(position.getRow(), position.getColumn());
+	}
+
+	public boolean ThereisAPiece(Position position) {
+		if (!PositionExists(position)) {
+			throw new BoardExeption("Posição não existente 3");}
+		return piece(position) != null;
+		
+
+	}
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,8 @@
 package chess;
 
 import boardgame.Board;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 
@@ -8,7 +10,7 @@ public class ChessMatch {
 	
 	public ChessMatch() {
 		board = new Board(8,8);
-		
+		inicialSetup();
 	}
 	public ChessPiece[][] getPieces(){
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
@@ -20,5 +22,17 @@ public class ChessMatch {
 		}
 		return mat;
 	
+		
 }
+	
+	private void placeNewPiece(char column , int row , ChessPiece piece ) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+	
+	private void inicialSetup() {
+		
+		placeNewPiece( 'b' ,6, new Rook(board, Color.WHITE));
+		placeNewPiece( 'e' ,8,new King(board,Color.BLACK));
+		placeNewPiece ('e' ,1, new King(board,Color.WHITE));
+	}
 }
